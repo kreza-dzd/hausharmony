b35b8a6a772afa338f91fe06fb941cdd85cacee9
Marko Krezic <markokrezic90@gmail.com>
2023-09-04 00:26:16 +0100

    Update AdviceView.vue

--- src/components/AdviceView.vue
+++ src/components/AdviceView.vue
@@ -1,13 +1,28 @@
 <template>
-  <h1>Advice</h1>
+  <div class="advice-view">
+   <img src="@/assets/advice.jpg" alt="Advice">
+   <p>Advice</p>
+ </div>
 </template>
 
 <script>
 export default {
-
-}
+ name: 'AdviceView',
+};
 </script>
 
 <style>
+.advice-view {
+ text-align: center;
+ color: white;
+}
+.advice-view img {
+ width: 50px;
+ height: 50px;
+ border-radius: 50%;
+}
 
+p {
+  font-weight: 600;
+}
 </style>
