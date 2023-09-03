93ea76a8d6f7b94c394d8a6f964e8188648c6e17
Marko Krezic <markokrezic90@gmail.com>
2023-09-04 00:26:22 +0100

    Update ProductsView.vue

--- src/components/ProductsView.vue
+++ src/components/ProductsView.vue
@@ -1,13 +1,28 @@
 <template>
-  <h1>Products</h1>
+  <div class="products-view">
+   <img src="@/assets/products.jpg" alt="Products">
+   <p>Products</p>
+ </div>
 </template>
 
 <script>
 export default {
-
-}
+ name: 'ProductsView',
+};
 </script>
 
 <style>
+.products-view {
+ text-align: center;
+ color: white;
+}
+.products-view img {
+ width: 50px;
+ height: 50px;
+ border-radius: 50%;
+}
 
+p {
+  font-weight: 600;
+}
 </style>
