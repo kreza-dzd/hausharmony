d5a03df9280c38d8c2136f6a0115c81464bada90
Marko Krezic <markokrezic90@gmail.com>
2023-09-04 00:26:25 +0100

    Update ProsView.vue

--- src/components/ProsView.vue
+++ src/components/ProsView.vue
@@ -1,13 +1,28 @@
 <template>
-  <h2>Pros</h2>
+   <div class="pros-view">
+    <img src="@/assets/pros.jpg" alt="Pros">
+    <p>Pros</p>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  name: 'ProsView',
+};
 </script>
 
 <style>
+.pros-view {
+  text-align: center;
+  color: white;
+}
+.pros-view img {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+}
 
+p {
+  font-weight: 600;
+}
 </style>
