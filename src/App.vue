b4dbde46384ce1e26ac246919c4d036facd90f08
Marko Krezic <68282076+kreza-dzd@users.noreply.github.com>
2023-09-03 09:27:41 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <header>
+      <input type="text" placeholder="Pretraži...">
+      <nav>
+        <IdeasView name="Ideas">Ideas</IdeasView>
+        <ProsView name="Pros">Pros</ProsView>
+        <ProductsView name="Products">Products</ProductsView>
+        <AdviceView name="Advice">Advice</AdviceView>
+      </nav>
+    </header>
+    <main>
+      <!-- Ovde možete dodati vaše komponente i sadržaj -->
+    </main>
+  </div>
+</template>
+
+<script>
+import IdeasView from './components/IdeasView.vue'
+import ProsView from './components/ProsView.vue'
+import ProductsView from './components/ProductsView.vue'
+import AdviceView from './components/AdviceView.vue'
+
+export default {
+  name: 'App',
+  components: {
+    IdeasView,
+    ProsView,
+    ProductsView,
+    AdviceView
+  }
+};
+</script>
+
+<style>
+body {
+  background-color: darkgrey;
+
+}
+#app {
+  font-family: Arial, sans-serif;
+}
+header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1em;
+  border-bottom: 1px solid #ccc;
+}
+nav {
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  margin-top: 1em;
+}
+input[type="text"] {
+  width: 100%;
+  padding: 1em;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+}
+</style>
---
+++ src/components/IdeasView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="ideas-view">
+    <img src="@/assets/ideas.jpg" alt="Ideas">
+    <p>Ideas</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IdeasView',
+};
+</script>
+
+<style>
+.ideas-view {
+  text-align: center;
+  color: white;
+}
+.ideas-view img {
+  width: 50px;
+  height: 50px;
+
+}
+</style>
